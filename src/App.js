aec7e82c4d0ea8456e24564006b288cd5d9ef2d6
Ciaran Edwards <ciaran.edwards@live.com>
2017-10-28 21:28:31 +0100

    Added the store to the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import SearchBar from './components/search_bar';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Five-Day Weather Forecast
-        <SearchBar />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          Five-Day Weather Forecast
+          <SearchBar />
+        </div>
+      </Provider>
     );
   }
 }
