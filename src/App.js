01bfe3b9a3ec16a8cabc4d8d3b1bc35b102fa0bf
Ciaran Edwards <ciaran.edwards@live.com>
2017-10-28 15:49:20 +0100

    Added a search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './components/search_bar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Five-Day Weather Forecast
+        <SearchBar />
       </div>
     );
   }
