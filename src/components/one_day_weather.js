eaf1cadda365338cd466cc4441e01274fe58c0c2
Ciaran Edwards <ciaran.edwards@live.com>
2017-10-29 15:01:03 +0000

    Added the weather info to OneDayWeather

--- src/components/one_day_weather.js
+++ src/components/one_day_weather.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
-const OneDayWeather = (props) => {
-  console.log("props in one day", props);
+const OneDayWeather = ({ data }) => {
+  const {
+    date, description, humidity, maxTemp, minTemp, pressure, windSpeed
+  } = data;
+
   return (
-    <div>
-      <p>I'm the OneDayWeather component</p>
+    <div className="one-day-weather">
+      <p className="date">{date}</p>
+      <div className="temperature-summary">
+        <p>Max: {maxTemp}&deg;</p>
+        <p>Min: {minTemp}&deg;</p>
+        <p>{description}</p>
+      </div>
+      <div className="more-details">
+        <p>Wind speed: {windSpeed} m/s</p>
+        <p>Humidity: {humidity}%</p>
+        <p>Pressure: {pressure} hPa</p>
+      </div>
     </div>
   );
 };
