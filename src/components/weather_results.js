0352389db42e3426b72ed1f2ac84f375995ea3a3
Ciaran Edwards <ciaran.edwards@live.com>
2017-10-29 14:30:47 +0000

    Converted the three-hour forecasts into daily forecasts for five days

--- src/components/weather_results.js
+++ src/components/weather_results.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import OneDayWeather from './one_day_weather';
@@ -6,8 +7,8 @@ class WeatherResults extends Component {
   renderForecast(forecast) {
     if (!forecast.city) return;
     const { city, list } = forecast;
-    console.log(forecast);
     const cityName = city.name;
+    const days = sortWeatherIntoDays(list);
 
     return (
       <div key={cityName}>
@@ -25,6 +26,72 @@ class WeatherResults extends Component {
       </div>
     );
   }
+}
+
+function sortWeatherIntoDays(list) {
+  const days = mergeDataForEachDay(list);
+  return simplifyDailyData(days);
+}
+
+function mergeDataForEachDay(list) {
+  const days = [];
+
+  _.each(list, (item) => {
+    const { dt_txt, wind, weather, main: {temp, pressure, humidity} } = item;
+    const tempInCelcius = Math.round(temp - 273.15);
+    const description = weather[0].description;
+    const date = dt_txt.split(' ')[0];
+    const dateObj = _.find(days, ['date', date]);
+
+    if (dateObj) {
+      dateObj.description.push( description );
+      dateObj.humidity.push( humidity );
+      dateObj.pressure.push( pressure );
+      dateObj.temps.push( tempInCelcius );
+      dateObj.windSpeed.push( wind.speed );
+    } else if (days.length < 5) {
+      days.push({ date,
+        'description': [ description ],
+        'humidity':    [ humidity ],
+        'pressure':    [ pressure ],
+        'temps':       [ tempInCelcius ],
+        'windSpeed':   [ wind.speed ]
+      });
+    }
+  });
+
+  return days;
+}
+
+function simplifyDailyData(days){
+  return _.each(days, (day) => {
+    const { description, humidity, pressure, temps, windSpeed } = day;
+    const sortedTemps = temps.sort((a, b) => { return a - b });
+
+    day.minTemp = sortedTemps[0];
+    day.maxTemp = sortedTemps[sortedTemps.length-1];
+
+    day.humidity = Math.round(_.sum(humidity) / humidity.length);
+    day.pressure = Math.round(_.sum(pressure) / pressure.length);
+    day.windSpeed = Math.round(_.sum(windSpeed) / windSpeed.length);
+
+    day.description = findMostCommon(description);
+  });
+}
+
+function findMostCommon(array) {
+  const counter = {};
+  let mostCommon = array[0], maxCount = 1;
+
+  _.each(array, el => {
+    (counter[el]) ? counter[el]++ : counter[el] = 1;
+    if (counter[el] > maxCount) {
+      mostCommon = el;
+      maxCount = counter[el];
+    }
+  });
+
+  return mostCommon;
 }
 
 function mapStateToProps({weather}) {
