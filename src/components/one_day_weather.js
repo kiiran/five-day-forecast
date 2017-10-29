d4c6ae4cc682ef24cdb117c5fcc9730a77b8ff54
Ciaran Edwards <ciaran.edwards@live.com>
2017-10-29 20:49:02 +0000

    Styled the weather forecast

--- src/components/one_day_weather.js
+++ src/components/one_day_weather.js
@@ -1,22 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const OneDayWeather = ({ data }) => {
   const {
     date, description, humidity, maxTemp, minTemp, pressure, windSpeed
   } = data;
+  const day = new Date(date).toLocaleDateString("en-GB", { weekday: 'short' });
+  const dateString = new Date(date).toLocaleDateString("en-GB", { day: 'numeric', month: 'short' });
 
   return (
     <div className="one-day-weather">
-      <p className="date">{date}</p>
-      <div className="temperature-summary">
-        <p>Max: {maxTemp}&deg;</p>
-        <p>Min: {minTemp}&deg;</p>
-        <p>{description}</p>
+      <div className="date">
+        <h3>{day}<br />{dateString}</h3>
       </div>
-      <div className="more-details">
-        <p>Wind speed: {windSpeed} m/s</p>
-        <p>Humidity: {humidity}%</p>
-        <p>Pressure: {pressure} hPa</p>
+      <div className="temperature-summary">
+        <div className="temp">
+          <span className="max-temp">{maxTemp}&deg;</span>
+          <span> / {minTemp}&deg;</span>
+        </div>
+        <p>{description}</p>
+        <table className="more-details">
+          <tbody>
+            <tr>
+              <td>
+                <i className="wi wi-strong-wind" />
+              </td>
+              <td>
+                {windSpeed} m/s
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <i className="wi wi-humidity" />
+              </td>
+              <td>
+                {humidity}%
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <i className="wi wi-barometer" />
+              </td>
+              <td>
+                {pressure}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
